6b42c7949d9fc38fc65d6e95249c3b6d4eb55223
reactgame <raghavrana997@gmail.com>
2023-09-21 17:58:29 +0530

    newupdateformenu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,7 +10,7 @@ export default function Menu() {
       <div className="app-menu">
 		  {/* Menu Bar */}
 			<ul className="app-menu-list">				
-				<li ><NavLink exact activeClassName="active_class" to="/">Products </NavLink></li>		
+				<li ><NavLink exact activeClassName="active_class" to="/">Products Main </NavLink></li>		
 				<li ><NavLink exact activeClassName="active_class" to="/programs">Programs </NavLink></li>		
 				<li ><NavLink exact activeClassName="active_class" to="/config">Config  </NavLink></li>		
 				<li ><NavLink exact activeClassName="active_class" to="/time">Time  </NavLink></li>		
